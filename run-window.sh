efae07c89a85205111575815a782a4da31640aca
Hanns Holger Rutz <contact@sciss.de>
2021-07-10 13:30:01 +0200

    re-force 640x480 <>

--- run-window.sh
+++ run-window.sh
@@ -2,15 +2,21 @@
 cd "$(dirname "$0")"
 echo "in|fibrillae (window)"
 
+# xrandr
+xrandr --output HDMI-1 --mode 640x480
+
 sleep 4
+# turn off screen blanking
 xset s off
 xset -dpms
 
+echo "- start qjackctl"
 qjackctl &
 
 sleep 10
+echo "- start mellite"
+mellite-launcher --headless --boot-audio --auto-run start /home/pi/Documents/projects/infibrillae/workspace.mllt &
 
-java -jar jvm/infibrillae.jar &
-
-mellite-launcher --headless --boot-audio --auto-run start /home/pi/Documents/projects/infibrillae/workspace.mllt
-
+sleep 30
+echo "- start visual"
+java -jar jvm/infibrillae.jar
